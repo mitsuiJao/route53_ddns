badf3cfb5e80fdd0dca718c0f71a00fa988aeb08
mitsuijao <nishima.kun@gmail.com>
2025-06-01 11:18:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## 使い方
+bashをよく読んで環境に合うように適宜変更してください！　←カス
+
+
+cronで5分おきに設定しておくといいと思います！
+```
+*/5 * * * * your/bash/path/ddns.sh >> your/bash/path/ddns.log 2>&1
+```
---
+++ ddns.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IP_FILE="$BASEDIR/current_ip"
+LOG_FILE="$BASEDIR/ip_change.log"
+JSON_FILE="$BASEDIR/changes.json"
+HOSTED_ZONE_ID="YOUR HOSTED ZONE ID"
+
+CURRENT_IP=$(curl -s inet-ip.info)
+
+if [ ! -f "$IP_FILE" ]; then
+  echo "" > "$IP_FILE"
+fi
+
+OLD_IP=$(cat "$IP_FILE")
+
+if [ "$CURRENT_IP" != "$OLD_IP" ]; then
+  echo " [bash] $(date '+%Y-%m-%d %H:%M:%S') IP changed from $OLD_IP to $CURRENT_IP"
+  echo "$CURRENT_IP" > "$IP_FILE"
+  jq --arg ip "$CURRENT_IP" '.Changes[0].ResourceRecordSet.ResourceRecords[0].Value = $ip' "$JSON_FILE" > tmp && mv tmp "$JSON_FILE"
+  aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file://$JSON_FILE \
+	  | jq -r '" [aws] Id: \(.ChangeInfo.Id)\n [aws] SubmittedAt: \(.ChangeInfo.SubmittedAt)"'
+fi
+
